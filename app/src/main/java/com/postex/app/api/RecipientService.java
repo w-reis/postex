a9a6f18653a556fb24b6417a9c37402251df886e
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-06-24 19:40:13 -0300

    cria a chamada da rota de cadastro

--- app/src/main/java/com/postex/app/api/RecipientService.java
+++ app/src/main/java/com/postex/app/api/RecipientService.java
@@ -1,4 +1,12 @@
 package com.postex.app.api;
 
+import com.postex.app.Recipient;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface RecipientService {
+    @POST("/recipients")
+    Call<Recipient> registerRecipient(@Body Recipient recipient);
 }
