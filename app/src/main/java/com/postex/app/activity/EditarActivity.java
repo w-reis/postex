ded846ff9e1d29f5b065c2d1a31c1db0bcf3656c
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-06-30 17:56:18 -0300

    ajusta estilo da pagina principal
    adicona action bar, adiciona pagina de edição

---
+++ app/src/main/java/com/postex/app/activity/EditarActivity.java
@@ -0,0 +1,16 @@
+package com.postex.app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.postex.app.R;
+
+public class EditarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar);
+    }
+}
--- app/src/main/java/com/postex/app/activity/PrincipalActivity.java
+++ app/src/main/java/com/postex/app/activity/PrincipalActivity.java
@@ -2,7 +2,11 @@ package com.postex.app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,10 +17,39 @@ public class PrincipalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().setElevation(0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         //Pega os dados do usuário
         Bundle data = getIntent().getExtras();
         Recipient recipient = (Recipient) data.getSerializable("recipient");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.logout:
+                Toast.makeText(getApplicationContext(), "Adicionar clicado", Toast.LENGTH_SHORT).show();
+                return (true);
+
+            case R.id.edit:
+                Intent tela = new Intent(PrincipalActivity.this, EditarActivity.class);
+                startActivity(tela);
+                return (true);
+
+        }
+        return (super.onOptionsItemSelected(item));
+    }
+
 }
+
