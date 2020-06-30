1bca9a173e36041199842bff4e85754b2e7046ba
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-06-29 23:05:18 -0300

    ajuste nas importações

--- app/src/main/java/com/postex/app/activity/ActivityCadastro.java
+++ app/src/main/java/com/postex/app/activity/ActivityCadastro.java
@@ -1,13 +1,13 @@
 package com.postex.app.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.postex.app.R;
 import com.postex.app.Recipient;
@@ -17,8 +17,6 @@ import com.postex.app.api.RecipientService;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ActivityCadastro extends AppCompatActivity {
 
