5371db9a43e4086043c10110e640b00160e52888
Wellinton Reis <wellinton_fabricio@hotmail.com>
2020-07-01 21:00:17 -0300

    adiciona rota para edição

--- app/src/main/java/com/postex/app/api/RecipientService.java
+++ app/src/main/java/com/postex/app/api/RecipientService.java
@@ -1,5 +1,6 @@
 package com.postex.app.api;
 
+import com.postex.app.CorrespondenceData;
 import com.postex.app.Recipient;
 
 import retrofit2.Call;
@@ -7,11 +8,23 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface RecipientService {
+    //rota para cadastro
     @POST("/recipients")
-    Call<Recipient> registerRecipient(@Body Recipient recipient);
+    Call<Void> registerRecipient(@Body Recipient recipient);
 
+    //rota editar
+    @PUT("/recipients/{id}")
+    Call<Void> updateRecipient(@Path("id") Integer id, @Body Recipient recipient, @Header("Authorization") String token);
+    //rota para login
     @GET("/login")
     Call<Recipient> login(@Header("Authorization") String authHeader);
+
+    //dados das correspondencias
+    @GET("/correspondences")
+    Call<CorrespondenceData> getCorrespondencesData(@Query("id_recipient") Integer id, @Header("Authorization") String token);
 }
